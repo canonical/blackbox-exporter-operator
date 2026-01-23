6d75f0818da148c727ff781ce01c8bbf2c0b46ce
Sina <sina.pahlavan@canonical.com>
2026-01-23 12:19:28 -0500

    fix: lint

--- tests/unit/test_peer_relation_data.py
+++ tests/unit/test_peer_relation_data.py
@@ -19,7 +19,7 @@ def test_peer_relation_data(context):
     # WHEN any event executes the reconciler.
     with context(context.on.update_status(), state=state) as mgr:
         state_out = mgr.run()
-        logger.info(state_out)
+
         peer_relation = next((obj for obj in state_out.relations if obj.endpoint == "peers"), None)
         unit_networks_data = json.loads(
             getattr(peer_relation, "local_unit_data", {}).get("unit-networks", [])
@@ -28,8 +28,7 @@ def test_peer_relation_data(context):
         # THEN the unit's networks' data must be written to remote unit data.
         assert unit_networks_data
 
-        # AND inside that unit networks data, there must be the iface, ip, and net keys
-        required_keys = ["iface", "ip", "net", "private-address", "egress-subnets", "principal-unit"]
+        # AND inside that unit networks data, there must be the iface, ip, and net and other keys
+        required_keys = ["iface", "ip", "net"]
         for interface in unit_networks_data:
             assert all(interface.get(k) for k in required_keys)
-
