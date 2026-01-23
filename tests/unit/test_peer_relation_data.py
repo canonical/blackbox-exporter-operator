68583d9af48392411077c7c41d89b2cb2572bd68
Sina <sina.pahlavan@canonical.com>
2026-01-23 12:13:01 -0500

    test: add peer relation unit test

---
+++ tests/unit/test_peer_relation_data.py
@@ -0,0 +1,35 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# To learn more about testing, see https://documentation.ubuntu.com/ops/latest/explanation/testing/
+
+
+import json
+import logging
+
+from scenario import PeerRelation, State
+
+logger = logging.getLogger(__name__)
+
+
+def test_peer_relation_data(context):
+    # GIVEN a BE with peers.
+    peer_relation = PeerRelation(endpoint="peers", peers_data={1: {}})
+    state = State(relations={peer_relation})
+    # WHEN any event executes the reconciler.
+    with context(context.on.update_status(), state=state) as mgr:
+        state_out = mgr.run()
+        logger.info(state_out)
+        peer_relation = next((obj for obj in state_out.relations if obj.endpoint == "peers"), None)
+        unit_networks_data = json.loads(
+            getattr(peer_relation, "local_unit_data", {}).get("unit-networks", [])
+        )
+
+        # THEN the unit's networks' data must be written to remote unit data.
+        assert unit_networks_data
+
+        # AND inside that unit networks data, there must be the iface, ip, and net keys
+        required_keys = ["iface", "ip", "net", "private-address", "egress-subnets", "principal-unit"]
+        for interface in unit_networks_data:
+            assert all(interface.get(k) for k in required_keys)
+
