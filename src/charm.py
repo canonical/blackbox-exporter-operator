fc94c8ffc3f8e6cd73388a3e2331d1085d94f77f
Sina <sina.pahlavan@canonical.com>
2026-01-28 08:43:49 -0500

    fix: status

--- src/charm.py
+++ src/charm.py
@@ -6,7 +6,6 @@
 
 import json
 import logging
-import os
 import socket
 from typing import Tuple, TypedDict
 
@@ -16,7 +15,7 @@ from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 from cosl.reconciler import all_events, observe_events
 from ops import CollectStatusEvent, StoredState
 from ops.jujucontext import JujuContext
-from ops.model import ActiveStatus, MaintenanceStatus, StatusBase
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, StatusBase
 
 from constants import DEFAULT_PORT, PEERS_RELATION_NAME, PROBES_RELATION_NAME
 from singleton_snap import SingletonSnapManager
@@ -30,6 +29,9 @@ from utils import get_unit_networks
 
 logger = logging.getLogger(__name__)
 
+def juju_context(arg: str):
+    """Return Juju env variables."""
+    return getattr(JujuContext.from_environ(), arg)
 
 def event() -> str:
     """Return Juju hook|action name.
@@ -38,16 +40,13 @@ def event() -> str:
     - https://github.com/juju/juju/blob/cbb05654c7444dd6bee29e49aff16339f02c34f9/docs/reference/action.md?plain=1#L55
     - https://github.com/juju/juju/blob/cbb05654c7444dd6bee29e49aff16339f02c34f9/docs/reference/hook.md?plain=1#L1088
     """
-    return os.environ.get("JUJU_HOOK_NAME") or os.environ.get("JUJU_ACTION_NAME", "")
+    return juju_context("hook_name")
 
 class CompositeStatus(TypedDict):
     """Per-component status holder."""
 
     # For the status of snap installation, start.
     snap: Tuple[str, str]
-
-    # For the status of relation data writes.
-    relations: Tuple[str, str]
 
 
 def to_tuple(status: StatusBase) -> Tuple[str, str]:
@@ -66,12 +65,10 @@ class BlackboxExporterOperatorCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        self._juju_ctx: JujuContext | None = None
 
         self._stored.set_default(
             status=CompositeStatus(
                 snap=to_tuple(ActiveStatus()),
-                relations=to_tuple(ActiveStatus()),
             )
         )
 
@@ -83,18 +80,6 @@ class BlackboxExporterOperatorCharm(ops.CharmBase):
 
         self.framework.observe(self.on.collect_unit_status, self._collect_unit_status)
         observe_events(self, all_events, self._reconcile)
-
-    @property
-    def juju_ctx(self) -> JujuContext:
-        """Juju execution context.
-
-        Provides access to a JujuContext derived from the current
-        environment, abstracting away direct access to Juju
-        environment variables.
-        """
-        if self._juju_ctx is None:
-            self._juju_ctx = JujuContext.from_environ()
-        return self._juju_ctx
 
     def _collect_unit_status(self, event: CollectStatusEvent):
         for status in self._stored.status.values():
@@ -147,11 +132,16 @@ class BlackboxExporterOperatorCharm(ops.CharmBase):
                     self.snap(snap_name).start(enable=True)
                     self._stored.status["snap"] = to_tuple(ActiveStatus())
                 except snap.SnapError as e:
+                    self._stored.status["snap"] = to_tuple(
+                        BlockedStatus("Unable to install the snap; see debug-log")
+                        )
                     raise SnapServiceError(f"Failed to start {snap_name}") from e
 
     def _remove_blackbox_exporter(self):
         """Coordinate blackbox-exporter snap and config file removal."""
-        self.unit.status = MaintenanceStatus("Removing Blackbox Exporter")
+        self._stored.status["snap"] = to_tuple(
+            MaintenanceStatus("Removing Blackbox Exporter")
+        )
         manager = SingletonSnapManager(self.unit.name)
         snap_name = "prometheus-blackbox-exporter"
         snap_revision = SnapMap.get_revision(snap_name)
@@ -171,7 +161,9 @@ class BlackboxExporterOperatorCharm(ops.CharmBase):
 
     def _remove_snap(self, snap_name: str):
         """Attempt to remove the snap."""
-        self.unit.status = MaintenanceStatus(f"Uninstalling {snap_name} snap")
+        self._stored.status["snap"] = to_tuple(
+            MaintenanceStatus(f"Uninstalling {snap_name} snap")
+        )
         try:
             self.snap(snap_name).ensure(state=snap.SnapState.Absent)
             logger.info(f"{snap_name} snap was uninstalled")
@@ -183,19 +175,16 @@ class BlackboxExporterOperatorCharm(ops.CharmBase):
     def _update_peer_relation_data(self):
         if not self.model.get_relation(PEERS_RELATION_NAME):
             return
-        self.unit.status = MaintenanceStatus("Updating peer relation data")
         peer_relation_data = {
-            "principal-unit": self.juju_ctx.principal_unit or "",
+            "principal-unit": juju_context("principal_unit") or "",
             "principal-hostname": socket.gethostname(),
             "unit-networks": json.dumps([n.to_dict() for n in get_unit_networks()]),
-            "az": self.juju_ctx.availability_zone or "",
+            "az": juju_context("availability_zone") or "",
         }
         relation = self.model.get_relation(PEERS_RELATION_NAME)
         assert relation is not None
 
         relation.data[self.unit].update(peer_relation_data)
-
-        self._stored.status["relations"] = to_tuple(ActiveStatus())
 
     def _generate_scrape_jobs(self):
         """Scrape jobs from peer relation data will be generated by this method."""
